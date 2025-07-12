1b918c1b8ec2e62cc16a9e87c515555b2a8a3bb4
Stoic-123 <longbigboss123@gmail.com>
2025-07-12 22:39:14 +0700

    fourt-commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mysql from "mysql2/promise";
 
 export const db = await mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "Lucifer168@$",
-  database: "ept", 
-  port: 3307,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
